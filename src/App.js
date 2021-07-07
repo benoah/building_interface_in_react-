1caa8b5cb000d97e53f69cda1fd0012712c53a07
ben <noahbenaas@gmail.com>
2021-07-07 13:39:51 +0200

    update

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import {BiArchive } from "react-icons/bi"
+import './index.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+<h1><BiArchive /> Your Appointment</h1>
+
+    </div>
+  );
+}
+
+export default App;
